8da7faa50dd05514cd18feac6872e8c68b049b8e
vimalkylm <131513067+vimalkylm@users.noreply.github.com>
2023-04-22 12:14:36 +0530

    script.js
    Add script.js

---
+++ script.js
@@ -0,0 +1,33 @@
+setTimeout(function() {
+    console.log(10);
+    setTimeout(function() {
+      console.log(9);
+      setTimeout(function() {
+        console.log(8);
+        setTimeout(function() {
+          console.log(7);
+          setTimeout(function() {
+            console.log(6);
+            setTimeout(function() {
+              console.log(5);
+              setTimeout(function() {
+                console.log(4);
+                setTimeout(function() {
+                  console.log(3);
+                  setTimeout(function() {
+                    console.log(2);
+                    setTimeout(function() {
+                      console.log(1);
+                      setTimeout(function() {
+                        console.log("Happy Independence Day");
+                      }, 1000);
+                    }, 1000);
+                  }, 1000);
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
